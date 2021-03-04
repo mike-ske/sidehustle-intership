9efe84e6817b86d987e4326054260fdffe960d51
mike-ske <66280654+mike-ske@users.noreply.github.com>
2021-03-04 23:06:22 +0100

    Athentication system in Php

---
+++ task_3/welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="head">
+        Welcome To Dashboard
+    </h1>
+
+    <a class="link" href="login.php">Back</a>
+</body>
+</html>
